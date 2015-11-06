11a588515bd00b3b7a1da281a9024fabbd5cb6ee
miguel <Miguel>
2015-11-06 14:04:29 +0000

    Added static database entries for dev, added mycomics and myboxes pages, began implementing queries

--- controllers/comics.py
+++ controllers/comics.py
@@ -1,14 +1,17 @@
 __author__ = 'miguel'
 
 
-def myboxes():
-
-    user_boxes = db(auth.user_id == db.comicbox.user_id).select('comicbox.id', 'comicbox.box_name')
+def mycomics():
 
     user_comics = db((auth.user_id == db.comicbox.user_id) &
-                   (db.comicbox.id == db.comicbook.box_id)).select()
+                   (db.comicbox.id == db.comicbook.box_id)).select(db.comicbook.id, db.comicbox.id, db.comicbox.box_name, db.comicbook.title, db.comicbook.cover, db.comicbook.issue_number, db.comicbook.publisher, db.comicbook.description)
+    return {'user_comics': user_comics}
 
-    return {'user_comics': user_comics, 'user_boxes': user_boxes}
+
+def myboxes():
+    user_boxes = db(auth.user_id == db.comicbox.user_id).select('comicbox.id', 'comicbox.box_name')
+
+    return {'user_boxes': user_boxes}
 
 
 def download():
