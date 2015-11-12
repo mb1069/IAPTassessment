145ab6afebc139c5ff6c0b0a7ff59903eca0045a
miguel <Miguel>
2015-11-12 20:50:03 +0000

    Fixed image preview on comicedit

--- static/js/formscripts.js
+++ static/js/formscripts.js
@@ -3,7 +3,7 @@ function readURL(input){
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function(e){
-            $('#image-preview').attr('src', e.target.result)
+            $('#preview-image').attr('src', e.target.result)
                 .width(100)
                 .height(130);
         };
